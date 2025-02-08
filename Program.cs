0fdc332eac03738dcbdd973ff10fe35edf61cf21
geyujia <16601163652@163.com>
2025-02-08 16:56:09 +0800

    添加手机号验证功能并移除示例输出 在 `Program.cs` 文件中： - 移除 `Console.WriteLine("Hello, World 1 !");` - 添加 `using System.Text.RegularExpressions;` - 添加字符串变量 `phoneNumber` 并赋值为 `"12345678900"` - 添加布尔变量 `isValid`，通过 `IsValidPhoneNumber(phoneNumber)` 判断手机号有效性 - 添加 `Console.WriteLine($"Phone number {phoneNumber} is valid: {isValid}");` 输出手机号是否有效 - 添加静态方法 `IsValidPhoneNumber` 使用正则表达式验证手机号 - 在 `IsValidPhoneNumber` 方法中，定义正则表达式模式 `@"^1[3-9]\d{9}$"` 并使用 `Regex.IsMatch(phoneNumber, pattern)` 匹配手机号

--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-Console.WriteLine("Hello, World 1 !");
+using System.Text.RegularExpressions;
+
+
+string phoneNumber = "12345678900";
+bool isValid = IsValidPhoneNumber(phoneNumber);
+Console.WriteLine($"Phone number {phoneNumber} is valid: {isValid}");
+
+// 正则表达式匹配手机号
+static bool IsValidPhoneNumber(string phoneNumber)
+{
+    string pattern = @"^1[3-9]\d{9}$";
+    return Regex.IsMatch(phoneNumber, pattern);
+}
+
 
 
